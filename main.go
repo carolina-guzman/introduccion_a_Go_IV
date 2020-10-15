867560a909cb738f8e1c96c3672e80ebbb46e3e6
carolina-guzman <carolina.guzman5480@alumnos.udg.mx>
2020-10-15 13:53:53 -0500

    Parte 2 introducción a go iv

--- main.go
+++ main.go
@@ -3,13 +3,12 @@ package main
 import "fmt"
 
 func fibonacci(i int, x int, res []int) int {
-	if i > x {
-		fmt.Println(res)
+	if i == x {
+		fmt.Println("Resultado: ", res)
 		return 0
 	}
 	if i == 0 { // caso base
-		res = append(res, 0)
-		res = append(res, 1)
+		res = append(res, 0, 1)
 		i++
 	} else {
 		res = append(res, res[i-1]+res[i-2])
@@ -18,7 +17,58 @@ func fibonacci(i int, x int, res []int) int {
 	return fibonacci(i+1, x, res)
 }
 
+func NumMayor(args ...int) int {
+	mayor := 0
+	for _, v := range args {
+		if v > mayor {
+			mayor = v
+		}
+	}
+
+	return mayor
+}
+
+func generadorImpares() func() uint {
+	i := uint(1)
+	return func() uint {
+		var par = i
+		i += 2
+		return par
+	}
+}
+
+func intercambia(a *int, b *int)  { 
+	*a , *b = *b , *a 
+}
+
 func main() {
 	var res []int
-	fibonacci(0, 10, res)
+	var n int
+	fmt.Println("\n--------------FIBONACCI----------------")
+	fmt.Println("Ingrese el la cantidad de numeros para la serie Fibonacci:")
+	fmt.Scan(&n)
+	fibonacci(0, n, res)
+
+	fmt.Println("\n--------------Numero mayor----------------")
+	a := []int{1,5,8,9,2,6,19,0}
+	fmt.Println("El numero mayor de: ", a, "es: ", NumMayor(a...))
+
+	fmt.Println("\n--------------Numeros Impares----------------")
+	nextPar := generadorImpares()
+	fmt.Println(nextPar())
+	fmt.Println(nextPar())
+	fmt.Println(nextPar())
+	fmt.Println(nextPar())
+	fmt.Println(nextPar())
+	fmt.Println(nextPar())
+	fmt.Println(nextPar())
+
+	fmt.Println("\n--------------Interbambiar numeros----------------")
+	a1 := 10
+	b1 := 3
+	fmt.Println("a: ", a1, ", b: ", b1)
+	intercambia(&a1, &b1)
+	fmt.Println("a: ", a1, ", b: ", b1)
+
+	
 }
